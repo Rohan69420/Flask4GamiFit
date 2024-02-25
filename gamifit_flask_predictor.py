67e8b8ca9d187af4cbbc4802d90858414667e8ae
Rohan <rohan13maharjan@gmail.com>
2024-02-25 09:30:36 +0545

    All intergrated to single file, addition of flask methods and routes remaining

--- gamifit_flask_predictor.py
+++ gamifit_flask_predictor.py
@@ -1,24 +1,14 @@
 # importing the required libaries
 from flask import Flask, render_template, request, redirect, url_for
-
-import tensorflow as tf
-
 import tensorflow.keras.backend as K
 from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing import image
-
-from tensorflow import keras
-from tensorflow.keras import models
-from tensorflow.keras.applications.inception_v3 import preprocess_input
-
-import numpy as np
+from classifier import predict_class
 from env_vars import model_rel_path, image_rel_path
+from get_image import download_image
+from query_db import get_data
+import os
 
 app = Flask(__name__)
-
-# mapping
-#necessary vars# picking 3 food items and generating separate data folders for the same
-food_list = ['samosa','pizza','omelette']
 
 # use the pre-trained model
 # Loading the best saved model to make predictions
@@ -27,33 +17,20 @@ model_best = load_model(model_rel_path,compile = False)
 
 
 # define the function to get the images from the url and predicted the class
+# Replace 'firebase_url' with your Firebase URL
+# Replace 'local_path.jpg' with your local file path
+firebase_url = "https://firebasestorage.googleapis.com/v0/b/gamifit69420.appspot.com/o/images%2Fpizza.jpg?alt=media&token=964badeb-e6a3-4b1e-84c0-619157f55072"
+download_image(firebase_url, 'capturedImage.jpg')
 
-
-
-#prediction function
-#resuming with the model trained on google colaboratory
-def predict_class(model, images, show = True):
-  for img in images:
-    img = image.load_img(img, target_size=(299, 299))
-    img = image.img_to_array(img)                    
-    img = np.expand_dims(img, axis=0)         
-    img = preprocess_input(img)                                      
-
-    pred = model.predict(img)
-    index = np.argmax(pred)
-    food_list.sort()
-    pred_value = food_list[index]
-    #print(pred)
-
-    #printing what the model predicted
-    print(pred_value) 
-    #we will return this value if needed, which we will need to anyway
 
 # Make a list of images and test the trained model
 #the path is always from the user root in C:
-images = []
-imagepath = image_rel_path
-images.append(imagepath+'samosa.jpg')
-#images.append(imagepath+'pizza.jpg')
-#images.append(imagepath+'omelette.jpg')
-predict_class(model_best, images, True)
+imagepath = ['capturedImage.jpg']
+pred_val = predict_class(model_best, imagepath, True)
+# Call the function with the value for FoodItem
+nutri_info =  get_data(pred_val)
+print(nutri_info)
+
+
+#clean the image after predicting
+os.remove("capturedImage.jpg")
