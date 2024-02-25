fb4a6eccf0065be1a4125d1cdefa249a2d48d504
Rohan <rohan13maharjan@gmail.com>
2024-02-25 09:30:03 +0545

    Only method included in get_image

--- get_image.py
+++ get_image.py
@@ -6,7 +6,4 @@ def download_image(firebase_url, local_path):
         with open(local_path, 'wb') as f:
             f.write(response.content)
 
-# Replace 'firebase_url' with your Firebase URL
-# Replace 'local_path.jpg' with your local file path
-firebase_url = "https://firebasestorage.googleapis.com/v0/b/gamifit69420.appspot.com/o/images%2F1708779357606392?alt=media&token=cfc64dea-3fe7-4521-b8a0-1c79e1bdba5a"
-download_image(firebase_url, 'capturedImage.jpg')
+
